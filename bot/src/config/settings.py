d605e9b5b65b37c20cf913f80d8fa17c2435ff2b
maksimzhohov <zhohovmaksim@gmail.com>
2024-06-29 15:23:35 +0300

    Added redis settings

--- bot/src/config/settings.py
+++ bot/src/config/settings.py
@@ -2,9 +2,17 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
+class RedisSettings(BaseSettings):
+    redis_host: str = Field(..., env="REDIS_HOST")
+    redis_port: str = Field(..., env="REDIS_PORT")
+    redis_password: str = Field(..., env="REDIS_PASSWORD")
+
+
 class Settings(BaseSettings):
     token: str = Field(..., env="TOKEN")
     parse_mode: str = Field(env="parse_mode")
+
+    redis: RedisSettings = RedisSettings()
 
     class Config:
         env_file = ".env"
