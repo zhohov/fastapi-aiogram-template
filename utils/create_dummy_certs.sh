928ce457e795e43a2201cccb559f01643c368eb1
maksimzhohov <zhohovmaksim@gmail.com>
2024-07-01 16:01:09 +0300

    Added certbot, docker-compose.prod.yaml and nginx for prod

---
+++ utils/create_dummy_certs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOMAIN="domain.com"
+DUMMY_CERT_DIR="/etc/letsencrypt/live/$DOMAIN"
+mkdir -p $DUMMY_CERT_DIR
+
+if [ ! -f "$DUMMY_CERT_DIR/privkey.pem" ] || [ ! -f "$DUMMY_CERT_DIR/fullchain.pem" ]; then
+  echo "Creating dummy certificates..."
+  openssl req -x509 -nodes -newkey rsa:2048 -days 1 -keyout "$DUMMY_CERT_DIR/privkey.pem" -out "$DUMMY_CERT_DIR/fullchain.pem" -subj "/CN=dummy.$DOMAIN"
+else
+  echo "Dummy certificates already exist."
+fi
+
+nginx -g "daemon off;"
